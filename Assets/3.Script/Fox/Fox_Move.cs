fa2d2ef9ae3115f3349ac085471091f39e821f24
백승주 <tmdwn3431@naver.com>
2024-08-28 15:37:21 +0900

    Player- Sprint 기능 구현

--- Assets/3.Script/Fox/Fox_Move.cs
+++ Assets/3.Script/Fox/Fox_Move.cs
@@ -28,65 +28,61 @@ public class Fox_Move : MonoBehaviour
 
     private void Update()
     {
-        
-        
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
         animator.SetFloat("move_right", moveHorizontal);
         animator.SetFloat("move_forward", moveVertical);
-        
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("isSprint", false);
+        }
+
         if (isDodgeing) return;
-        // 이동 방향 설정
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
 
         // 이동 처리
-        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
-        
+            rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+
         // 플레이어 회전 처리
-        if (movement != Vector3.zero)
+        if (movement != Vector3.zero&&!animator.GetBool("isFocus"))
         {
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            rb.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-        
         //집중 상태
         if (Input.GetKey(KeyCode.LeftShift))
         {
             animator.SetBool("isFocus", true);
         }
         else
-        { 
+        {
             animator.SetBool("isFocus", false);
         }
 
         //구르기
-        //if(isInput)
         if (isInput && Input.GetKeyDown(KeyCode.Space))
         {
-            animator.applyRootMotion = true;
+            //animator.applyRootMotion = true;
             isDodgeing = true;
             isInput = false;
             animator.SetTrigger("dodge");
         }
 
         //달리기
-        if (isInput && Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             animator.SetBool("isSprint", true);
         }
-        else
-        {
-            animator.SetBool("isSprint", false);
-        }
-
+       
 
     }
 
     //애니메이션 이벤트클립
     #region
-    public void Dodge_RootMotion_Finish()
+    public void Dodge_Input_Finish()
     {
-            animator.applyRootMotion = false;
         isInput = true;
     }
     public void Dodge_Finish()
