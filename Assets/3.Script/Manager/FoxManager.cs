d3b86dd5e13a642af04e56801417bd641d86d63f
백승주 <tmdwn3431@naver.com>
2024-09-25 21:13:03 +0900

    SwordCave -> World 1 씬 이동 수정해야 함
    Fox를 미리 배치하는게 아닌 스폰매니저를 만들어 Fox를 생성하게 해야 함

--- Assets/3.Script/Manager/FoxManager.cs
+++ Assets/3.Script/Manager/FoxManager.cs
@@ -6,13 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class FoxManager : MonoBehaviour
 {
-    public static FoxManager Instance { get; private set; }
+    public static FoxManager Instance = null;
+ 
     private Animator ani;
+    private Fox_Move fox_Move;
 
     //체력관련
     public int maxHealth = 10;
     public int currentHealth;
+    
     private bool isHurt = false;
+    private bool fromSwordCave; //SwordCave씬 -> World
 
     //스테미너 관련
     public float maxStamina = 40f; //스테미너 최대
@@ -31,19 +35,31 @@ public class FoxManager : MonoBehaviour
     {
         if (Instance == null)
         {
+            GameObject fox = GameObject.FindGameObjectWithTag("Fox");
+            if (fox != null)
+            { 
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            Debug.Log("새로 생성 됨");
+            }
             
         }
         else
         {
             Destroy(gameObject);
-            Debug.Log("FoxManager 생성 안 됨");
+            Debug.Log("FoxManager 파괴");
+            return;
         }
-
+        SceneManager.sceneLoaded += OnSceneLoaded;
         ani = GetComponent<Animator>();
+        fox_Move = GetComponent<Fox_Move>();
+        
     }
-
+    //private void OnDestroy()
+    //{
+    //    SceneManager.sceneLoaded -= OnSceneLoaded; // 씬 로드 이벤트 구독 해제
+    //    Debug.Log("dddd");
+    //}
     private void Start()
     {
         currentHealth = maxHealth;
@@ -52,7 +68,10 @@ public class FoxManager : MonoBehaviour
         nowStamina = maxStamina;
         OnStaminaChanged?.Invoke(nowStamina, maxStamina);
 
+        
+        
         Debug.Log(gameObject.transform.position);
+            
     }
 
     private void Update()
@@ -65,18 +84,6 @@ public class FoxManager : MonoBehaviour
         OnStaminaChanged?.Invoke(nowStamina, maxStamina);
     }
     
-    //public void TakeDamage(int damage)
-    //{
-    //    currentHealth -= damage;
-    //    currentHealth = Math.Clamp(currentHealth, 0, maxHealth);
-    //    OnHealthChanged?.Invoke(currentHealth, maxHealth);
-
-    //    if (currentHealth <= 0)
-    //    {
-    //        Die();
-    //    }
-    //}
-
     public void Heal(int amount)
     {
         currentHealth += amount;
@@ -106,6 +113,32 @@ public class FoxManager : MonoBehaviour
             OnStaminaChanged?.Invoke(nowStamina, maxStamina);
         }
     }
+
+    //다른 씬 들어갈 때
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "SwordCave" && !fromSwordCave)
+        {
+            Debug.Log("SwordCave");
+            gameObject.transform.position = new Vector3(5.6f, 5.9f, -4.9f);
+            fromSwordCave = true;
+            //SceneManager.sceneLoaded -= OnSceneLoaded;
+            Debug.Log("OnSceneLoaded 구독 해제됨"); // 구독 해제 확인 로그
+        }
+        else if (scene.name == "World 1" && fromSwordCave)
+        {
+            gameObject.transform.position = new Vector3(-10.8f, 12.0f, -128.5f);
+            Debug.Log("World 1");
+            fromSwordCave = false;
+            fox_Move.canMoveOutNav = true;
+            //SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        //구독 해지
+
+    }
+
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Monster_Attack"))
@@ -149,5 +182,19 @@ public class FoxManager : MonoBehaviour
                 Debug.LogError("SaveSystem.Instance is null!");
             }
         }
+
+        if (other.CompareTag("SwordCave"))
+        {
+            SceneManager.LoadScene("SwordCave");
+            fromSwordCave = false;
+        }
+
+        if (other.CompareTag("SwordCave_World"))
+        {
+            SceneManager.LoadScene("World 1");
+            fox_Move.canMoveOutNav = false;
+            
+        }
     }
+    
 }
